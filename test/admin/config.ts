b5f07e196434ad643e775e200b5ac96997da9657
Dan Ribbens <dan.ribbens@gmail.com>
2023-03-02 15:24:18 -0500

    chore: admin demo

--- test/admin/config.ts
+++ test/admin/config.ts
@@ -1,14 +1,5 @@
-import path from 'path';
-import { mapAsync } from '../../src/utilities/mapAsync';
 import { devUser } from '../credentials';
 import { buildConfig } from '../buildConfig';
-import AfterDashboard from './components/AfterDashboard';
-import CustomMinimalRoute from './components/views/CustomMinimal';
-import CustomDefaultRoute from './components/views/CustomDefault';
-import BeforeLogin from './components/BeforeLogin';
-import AfterNavLinks from './components/AfterNavLinks';
-import { slug, globalSlug } from './shared';
-import Logout from './components/Logout';
 
 export interface Post {
   id: string;
@@ -19,144 +10,48 @@ export interface Post {
 }
 
 export default buildConfig({
-  admin: {
-    css: path.resolve(__dirname, 'styles.scss'),
-    components: {
-      // providers: [CustomProvider, CustomProvider],
-      routes: [
-        {
-          path: '/custom-minimal-route',
-          Component: CustomMinimalRoute,
-        },
-        {
-          path: '/custom-default-route',
-          Component: CustomDefaultRoute,
-        },
-      ],
-      afterDashboard: [
-        AfterDashboard,
-      ],
-      beforeLogin: [
-        BeforeLogin,
-      ],
-      logout: {
-        Button: Logout,
-      },
-      afterNavLinks: [
-        AfterNavLinks,
-      ],
-      views: {
-        // Dashboard: CustomDashboardView,
-        // Account: CustomAccountView,
-      },
-    },
-  },
-  i18n: {
-    resources: {
-      en: {
-        general: {
-          dashboard: 'Home',
-        },
-      },
-    },
-  },
   collections: [
     {
-      slug: 'users',
-      auth: true,
-      fields: [],
-    },
-    {
-      slug,
-      labels: {
-        singular: {
-          en: 'Post en',
-          es: 'Post es',
-        },
-        plural: {
-          en: 'Posts en',
-          es: 'Posts es',
-        },
-      },
-      admin: {
-        description: { en: 'Description en', es: 'Description es' },
-        listSearchableFields: ['title', 'description', 'number'],
-        group: { en: 'One', es: 'Una' },
-        useAsTitle: 'title',
-        defaultColumns: ['id', 'number', 'title', 'description'],
-      },
+      slug: 'posts',
       fields: [
         {
           name: 'title',
-          label: {
-            en: 'Title en',
-            es: 'Title es',
-          },
           type: 'text',
         },
         {
-          name: 'description',
-          type: 'text',
+          name: 'category',
+          type: 'relationship',
+          relationTo: 'categories',
         },
         {
-          name: 'number',
-          type: 'number',
+          name: 'author',
+          type: 'relationship',
+          relationTo: 'users',
         },
         {
-          name: 'richText',
-          type: 'richText',
-          admin: {
-            elements: [
-              'relationship',
-            ],
-          },
+          name: 'publishedAt',
+          type: 'date',
+        },
+        {
+          name: 'status',
+          type: 'select',
+          options: [
+            { label: 'Draft', value: 'draft' },
+            { label: 'Ready for review', value: 'review' },
+            { label: 'Ready to publish', value: 'publish' },
+            { label: 'Published', value: 'published' },
+          ],
         },
       ],
     },
     {
-      slug: 'group-one-collection-ones',
+      slug: 'categories',
       admin: {
-        group: { en: 'One', es: 'Una' },
+        useAsTitle: 'name',
       },
       fields: [
         {
-          name: 'title',
-          type: 'text',
-        },
-      ],
-    },
-    {
-      slug: 'group-one-collection-twos',
-      admin: {
-        group: { en: 'One', es: 'Una' },
-      },
-      fields: [
-        {
-          name: 'title',
-          type: 'text',
-        },
-      ],
-    },
-    {
-      slug: 'group-two-collection-ones',
-      admin: {
-        group: 'Two',
-      },
-      fields: [
-        {
-          name: 'title',
-          type: 'text',
-        },
-      ],
-    },
-    {
-      slug: 'group-two-collection-twos',
-      admin: {
-        group: 'Two',
-      },
-      fields: [
-        {
-          name: 'title',
+          name: 'name',
           type: 'text',
         },
       ],
@@ -164,38 +59,8 @@ export default buildConfig({
   ],
   globals: [
     {
-      slug: globalSlug,
-      label: {
-        en: 'Global en',
-        es: 'Global es',
-      },
-      admin: {
-        group: 'Group',
-      },
-      fields: [
-        {
-          name: 'title',
-          type: 'text',
-        },
-      ],
-    },
-    {
-      slug: 'group-globals-one',
-      admin: {
-        group: 'Group',
-      },
-      fields: [
-        {
-          name: 'title',
-          type: 'text',
-        },
-      ],
-    },
-    {
-      slug: 'group-globals-two',
-      admin: {
-        group: 'Group',
-      },
+      slug: 'settings',
+      label: 'Settings',
       fields: [
         {
           name: 'title',
@@ -216,15 +81,5 @@ export default buildConfig({
     } catch (error) {
       // continue
     }
-
-    await mapAsync([...Array(11)], async () => {
-      await payload.create({
-        collection: slug,
-        data: {
-          title: 'title',
-          description: 'description',
-        },
-      });
-    });
   },
 });
